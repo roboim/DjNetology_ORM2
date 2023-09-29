ae856a6bfe6e5449a424afdf2227eec43663690c
Ilya Shevelev <robotedu@yandex.ru>
2023-09-29 17:29:52 +0300

    feat: all mandatory tasks for goal migration completed

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -3,11 +3,19 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class StudentTeacherInline(admin.TabularInline):
+    model = Student
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     pass
+    # list_display = ['id', 'name', 'group']
+    # list_filter = ['group']
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     pass
+    # list_display = ['id', 'name', 'subject', 'students']
+    # list_filter = ['subject']
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,15 +1,17 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
     context = {}
 
+
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
+    # ordering = 'group'
+    students_db = Student.objects.all().order_by('group')
+    context = {'object_list': students_db}
     return render(request, template, context)
